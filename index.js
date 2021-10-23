4f04931caddac41aa741960f8993513cbc7f665e
jsooter <jsooter.dev@gmail.com>
2021-10-23 11:18:36 -0700

    fix variable scopes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+let payload;
+let committer_email;
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -8,14 +11,14 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  payload = JSON.stringify(github.context.payload, undefined, 2)
 //   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
 
 try {
-    let committer_email = payload.head_commit.email;
+    committer_email = payload.head_commit.email;
     console.log(`Committer email: ${committer_email}`);
     if (payload.head_commit.email != 'josh-sooter@pluralsight.com') {
         core.setFailed(`User email ${committer_email} is is not compliant`);
